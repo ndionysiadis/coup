f3f11072702c8b6d9e6a3722641749b8ec2e9b52
ndionysiadis <nicolasdionysiadis@outlook.com>
2024-08-28 17:42:45 +0300

    COUP-5

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use Inertia\Response;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function  __invoke(): Response
     {
         return Inertia::render('Index');
     }
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import {Head} from "@inertiajs/vue3";
 
+const title = 'COUP - Coffee, Wine, Kitchen'
 </script>
 
 <template>
+    <Head :title="title"/>
 
 </template>
