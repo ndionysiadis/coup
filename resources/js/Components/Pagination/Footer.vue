d6914683b8aa7f0120383626defca596ba6cd173
ndionysiadis <nicolasdionysiadis@outlook.com>
2024-10-17 14:31:13 +0300

    COUP-16

---
+++ resources/js/Components/Pagination/Footer.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Copyrights from "@/Components/Copyrights.vue";
+import {
+    PhFacebookLogo,
+    PhInstagramLogo,
+    PhMapPin,
+    PhPhone,
+} from "@phosphor-icons/vue";
+import AppLink from "@/Components/Links/AppLink.vue";
+</script>
+
+<template>
+    <footer class="flex flex-col items-center justify-center gap-2">
+        <div class="flex items-center gap-4">
+            <AppLink href="https://www.facebook.com/CoupEspressoWine" blank>
+                <PhFacebookLogo
+                    :size="32"
+                    weight="fill"
+                    class="mt-0.5 text-gray-600 hover:text-gray-400"
+                />
+            </AppLink>
+
+            <AppLink href="https://www.instagram.com/coupespressowinebar" blank>
+                <PhInstagramLogo
+                    :size="32"
+                    weight="fill"
+                    class="mt-0.5 text-gray-600 hover:text-gray-400"
+                />
+            </AppLink>
+
+            <AppLink href="https://maps.app.goo.gl/zjSKnrGaE43eZSJb9" blank>
+                <PhMapPin
+                    :size="32"
+                    weight="fill"
+                    class="mt-0.5 text-gray-600 hover:text-gray-400"
+                />
+            </AppLink>
+
+            <AppLink href="callto:2384501161" blank>
+                <PhPhone
+                    :size="32"
+                    weight="fill"
+                    class="mt-0.5 text-gray-600 hover:text-gray-400"
+                />
+            </AppLink>
+        </div>
+        <Copyrights />
+    </footer>
+</template>
--- resources/js/Layouts/LandingLayout.vue
+++ resources/js/Layouts/LandingLayout.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Menu from "@/Components/Pagination/Menu.vue";
+import Footer from "@/Components/Pagination/Footer.vue";
 </script>
 
 <template>
     <div class="relative z-10">
         <Menu />
+
+        <Footer />
     </div>
 </template>
 
